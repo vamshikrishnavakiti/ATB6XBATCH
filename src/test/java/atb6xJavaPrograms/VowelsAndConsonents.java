8f1022bdbb00630a85e2a0389fb7b84f7f934b93
Vamshi Krishna <vamshi.qaeng@gmail.com>
2024-04-23 15:04:22 +0530

    Vovels and consonnat

---
+++ src/test/java/atb6xJavaPrograms/VowelsAndConsonents.java
@@ -0,0 +1,25 @@
+package atb6xJavaPrograms;
+
+import java.util.Scanner;
+
+public class VowelsAndConsonents {
+
+	public static void main(String[] args) {
+Scanner sc = new Scanner(System.in);
+   System.out.println("Enter Charector");
+   char ch =sc.next().charAt(0);
+   if((ch=='a')|| (ch=='e')||(ch=='i')||(ch=='o')||(ch=='u')){
+	   System.out.println("Enter Char is Vowel");
+   }
+   else if((ch=='A')|| (ch=='E')||(ch=='I')||(ch=='O')||(ch=='U')) {
+	   System.out.println("Entered Char is Vowel");
+   }
+   else if((ch<'a' && ch>'z') || (ch <'A') &&(ch>'Z')) {
+	   System.out.println("Invalit Entry");
+   }
+   else {
+	   System.out.println("Consonant");
+   }
+	}
+
+}
