a1ac37f74c8e297c077e7a4322e17c347a58ca90
Vamshi Krishna <vamshi.qaeng@gmail.com>
2024-04-23 14:26:10 +0530

    initial commit

---
+++ src/main/java/ATB6XBATCH/ATB6XBATCH/App.java
@@ -0,0 +1,13 @@
+package ATB6XBATCH.ATB6XBATCH;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/atb6xJavaPrograms/Helloworld.java
@@ -0,0 +1,9 @@
+package atb6xJavaPrograms;
+
+public class Helloworld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+
+}
