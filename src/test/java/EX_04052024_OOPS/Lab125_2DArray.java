129bf87b65bb5405b079cd6ff5fc0655c64ac5f4
Vamshi Krishna <vamshi.qaeng@gmail.com>
2024-05-04 13:02:29 +0530

    Modified 2d Array

--- src/test/java/EX_04052024_OOPS/Lab125_2DArray.java
+++ src/test/java/EX_04052024_OOPS/Lab125_2DArray.java
@@ -10,7 +10,7 @@ public class Lab125_2DArray {
 				{31,32,33}
 		};
 		//Normal initialization.
-		int [][] array_2d2=new  int[3][3];
+		/*int [][] array_2d2=new  int[3][3];
 		 array_2d2[0][0]=11;
 		 array_2d2[0][1]=12;
 		 array_2d2[0][2]=13;
@@ -19,13 +19,14 @@ public class Lab125_2DArray {
 		 array_2d2[1][3]=23;
 		 array_2d2[2][0]=31;
 		 array_2d2[2][1]=32;
-		 array_2d2[3][2]=33;
+		 array_2d2[3][2]=33; */
 		 
 		 //Print the 2d_Array
-		 for (int i=0;i<array2d.length;i++) {
-			 for(int j=0;j<array2d[i].length;j++) {
-				System.out.println(array2d[i][j]); 
+		 for (int i=0;i<=2;i++) {
+			 for(int j=0;j<=2;j++) {
+				System.out.print(array2d[i][j]+" "); 
 			 }
+			 System.out.println();
 		 }
 		
 	}
